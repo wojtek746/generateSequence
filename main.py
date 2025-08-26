a7f77fa462420fd48b4850875d54779112b40a1f
wojtek746 <Wojtek7462@gmail.com>
2025-08-26 09:02:54 +0200

    printing everything with fractions

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from fractions import Fraction
+
 def main():
     n = int(input("Enter how many numbers the sequence should have: "))
     numbers = []
     for i in range(1, n+1):
-        a = float(input(f"Enter a{i}:"))
-        numbers.append(a)
+        numbers.append(Fraction(input(f"Enter a{i}:")))
 
-    factors = [0.0] * n
+    factors = [Fraction(0)] * n
     for i in range(n):
-        denominator = 1
+        denominator = Fraction(1)
         for j in range(n):
             if j != i:
-                denominator *= (i+1 - (j + 1))
+                denominator *= Fraction(i+1 - (j + 1))
 
-        polynomial = [1.0]
+        #polinomial is (x-1)(x-2)...(x-n) without (x-i)
+        polynomial = [Fraction(1)]
         for j in range(n):
             if j != i:
-                new = [0.0] * (len(polynomial) + 1)
+                new = [Fraction(0)] * (len(polynomial) + 1)
                 for k in range(len(polynomial)):
-                    new[k] += polynomial[k] * (-(j+1))
+                    new[k] += polynomial[k] * Fraction(-(j+1))
                     new[k+1] += polynomial[k]
                 polynomial = new
 
@@ -30,22 +32,35 @@ def main():
     first = True
     for i in range(len(factors) - 1, -1, -1):
         factor = factors[i]
+        if factor == 0:
+            continue
+
+        if factor.denominator == 1:
+            factor_str = str(factor.numerator)
+        else:
+            factor_str = f"{factor.numerator}/{factor.denominator}"
+
         if not first:
-            result += " + "
+            if factor > 0:
+                result += " + "
+            else:
+                result += " - "
+                factor_str = factor_str[1:]
         else:
             first = False
+
         if i == 0:
-            result += f"{factor}"
+            result += factor_str
         elif i == 1:
             if factor == 1:
                 result += "x"
             else:
-                result += f"{factor}x"
+                result += f"{factor_str}x"
         else:
             if factor == 1:
                 result += f"x^{i}"
             else:
-                result += f"{factor}x^{i}"
+                result += f"{factor_str}x^{i}"
     return result
 
 if __name__ == "__main__":
